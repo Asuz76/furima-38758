a5d558e7e047c0090270861bd1d7951753bad62c
Asuz76 <115327794+Asuz76@users.noreply.github.com>
2023-02-12 23:12:47 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,12 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
 end
