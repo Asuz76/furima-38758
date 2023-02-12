610a60b776b727e5379263c20f32c37c245c532c
Asuz76 <115327794+Asuz76@users.noreply.github.com>
2023-02-12 22:07:13 +0900

    active hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user, class_name: "User"
   has_one_attached :image
 
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to :category
-  # belongs_to :status
-  # belongs_to :fee
-  # belongs_to :prefecture
-  # belongs_to :delivery
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :delivery
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
