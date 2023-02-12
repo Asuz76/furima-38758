e64ffe3fc06cae1199fd718edd2b31c082d093c5
Asuz76 <115327794+Asuz76@users.noreply.github.com>
2023-02-12 22:25:52 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if
+      @item.save
+      redirect_to root_path
+    else
+      redirect_to new_item_path
+    end
+  end
+ 
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:text,:category_id, :status_id, :fee_id, :prefecture_id, :delivery_id, :image, :price).merge(user_id: current_user.id)
+  end
+
 end
