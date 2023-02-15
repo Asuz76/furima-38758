61f6f379ba9f2e85ca2bb8173803371e483a6996
Asuz76 <115327794+Asuz76@users.noreply.github.com>
2023-02-15 19:50:31 +0900

    updateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -26,6 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:id])
     item.update(item_params)
     redirect_to  controller: :items, action: :show
   end
