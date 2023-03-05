5be44cf0c047247035261f0ab593923f8c0d17b2
Asuz76 <115327794+Asuz76@users.noreply.github.com>
2023-03-05 13:27:17 +0900

    アクセス権修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @item = Item.find(params[:item_id])
-    @purchaseform = Purchaseform.new
+    if @item.user_id != current_user.id && @item.order == nil
+      @purchaseform = Purchaseform.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
